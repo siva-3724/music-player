bb85719e5f5d12bafd7546fd51f430a47f93665d
Sivaprasath P <psivaprasath4@gmail.com>
2025-03-07 16:03:55 +0530

    Add files via upload

---
+++ Music Player in JavaScript/README.md
@@ -0,0 +1,7 @@
+# music player
+# The name of the music player.
+# Features (e.g., play, pause, playlist support).
+# Technology used (e.g., HTML, CSS, JavaScript).
+# Future enhancements planned.
+
+
---
+++ Music Player in JavaScript/js/music-list.js
@@ -0,0 +1,56 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Aarum Kaanaathinnen ",
+    artist: "Vineeth Sreenivasan",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Edi Penne Freak Penne",
+    artist: "Sathyajith, Neethu Naduvathettu",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Maahiya",
+    artist: " Hesham Abdul Wahab, Jeenu Nazeer",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Munnaale Ponaale",
+    artist: "Shaan Rahman",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Manikya Malaraya",
+    artist: "Vineeth Sreenivasan",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Mannum Mazhayum",
+    artist: "Shafi Kollam",
+    img: "music-6",
+    src: "music-6"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
